3c4b3e06a26fcb60db020931ab9e99967d585ec8
0sunset0 <chi6465618@naver.com>
2026-03-26 10:47:30 +0900

    Initial Commit

---
+++ challenge1/challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge1App.swift
+//  challenge1
+//
+//  Created by 노을 on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
